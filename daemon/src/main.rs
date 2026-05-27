2818cf8c17973a73c4c10d2594c980ef800ad349
Mythic1x <coolcaleb1120@gmail.com>
2026-05-27 18:50:59 -0400

    fix username handling

--- daemon/src/main.rs
+++ daemon/src/main.rs
@@ -28,8 +28,8 @@ pub enum VcpMessage {
 }
 
 impl VcpMessage {
-    fn parse(bytes: &[u8; 1500]) -> Result<Self, String> {
-        if bytes.starts_with(b"Packet/")  {
+    fn parse(bytes: &[u8]) -> Result<Self, String> {
+        if bytes.starts_with(b"PACKET/")  {
            let packet_nr_bytes = &bytes[7..15];
            let packet_nr = u64::from_be_bytes(packet_nr_bytes.try_into().map_err(|_| "Malformed packet found when parsing packet number".to_string())?);
            let data_len_bytes = &bytes[15..17]; 
@@ -45,7 +45,10 @@ impl VcpMessage {
                     let str_port = args.next().ok_or("Malformed Packet: Missing port")?;
                     let port = str_port.parse::<u16>().map_err(|_| "Invalid port number")?;
                     let mimetype = args.next().ok_or("Malformed Packet: Missing Mimetype")?.to_string();
-                    let username = args.next().ok_or("Malformed Packet: Missing Username")?.to_string();
+                    let username = args.collect::<Vec<&str>>().join(" ").replace('"', "");
+                    if username.is_empty() {
+                        return Err("Malformed Packet: Missing Username".to_string())
+                    } 
                     return Ok(VcpMessage::Call { ip: ip.to_string(), port, mimetype, username });
                 }
                 (Some("ACCEPT"), Some("CALL")) => {
@@ -53,7 +56,11 @@ impl VcpMessage {
                     let str_port = args.next().ok_or("Malformed Packet: Missing port")?;
                     let port = str_port.parse::<u16>().map_err(|_| "Invalid port number")?;
                     let mimetype = args.next().ok_or("Malformed Packet: Missing Mimetype")?.to_string();
-                    let username = args.next().ok_or("Malformed Packet: Missing Username")?.to_string();
+                    args.next().ok_or("Malformed Packet: Missing Username")?;
+                    let username = args.collect::<Vec<&str>>().join(" ").replace('"', "");
+                     if username.is_empty() {
+                        return Err("Malformed Packet: Missing Username".to_string())
+                    } 
                     return Ok(VcpMessage::AcceptCall { ip, port, mimetype, username });
                 }
                 (Some("DECLINE"), Some("CALL")) => {
@@ -61,7 +68,11 @@ impl VcpMessage {
                     let str_port = args.next().ok_or("Malformed Packet: Missing port")?;
                     let port = str_port.parse::<u16>().map_err(|_| "Invalid port number")?;
                     let mimetype = args.next().ok_or("Malformed Packet: Missing Mimetype")?.to_string();
-                    let username = args.next().ok_or("Malformed Packet: Missing Username")?.to_string();
+                    args.next().ok_or("Malformed Packet: Missing Username")?;
+                    let username = args.collect::<Vec<&str>>().join(" ").replace('"', "");
+                     if username.is_empty() {
+                        return Err("Malformed Packet: Missing Username".to_string())
+                    } 
                     return Ok(VcpMessage::DeclineCall { ip, port, mimetype, username });
                 }
                 _ => Err("Unknown Command or Malformed Packet".to_string()),
@@ -80,9 +91,19 @@ async fn main() -> io::Result<()> {
     loop {
         match sock.recv_from(&mut buf).await {
             Ok((data, addr)) => {
-                match VcpMessage::parse(&buf) {
+                match VcpMessage::parse(&buf[..data]) {
                     Ok(res) => {
-                      todo!()  
+                      match res {
+                        VcpMessage::Call { ip, port, mimetype, username } => {
+                            let text = format!("Incoming Call from {ip} port {port} with mimetype {mimetype} and username {username}");
+                            println!("{text}");
+                            let bytes: &[u8] = text.as_bytes();
+                            sock.send_to(bytes, addr).await?;
+                        }
+                        VcpMessage::AcceptCall { ip, port, mimetype, username } => todo!(),
+                        VcpMessage::DeclineCall { ip, port, mimetype, username } => todo!(),
+                        VcpMessage::Packet { packet_nr, data_len, data } => todo!(),
+                        }  
                     }
                     Err(err) => println!("{err}"),
                 }
